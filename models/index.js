0b2490e8aa0ab2bcc62227793de6f7c7018d674b
SplitRabbit <jue.kun.liu@gmail.com>
2022-06-13 09:50:24 -0400

    changed model structure

--- models/index.js
+++ models/index.js
@@ -4,6 +4,9 @@ const User = require('./User');
 const Comment = require('./Comment');
 
 // create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
@@ -11,6 +14,16 @@ Post.belongsTo(User, {
 });
 
 Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'SET NULL'
 });
